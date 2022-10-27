de54cc93517e38cc8c415175f26bc52e93203a07
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-26 21:22:09 -0500

    Update SharkPontoon.java

--- codewars/8 kyu/SharkPontoon.java
+++ codewars/8 kyu/SharkPontoon.java
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/57e921d8b36340f1fd000059
+
 public class SharkPontoon {
   public static String shark(double pontoonDistance,
                              double sharkDistance, double youSpeed, double sharkSpeed, boolean dolphin) {
