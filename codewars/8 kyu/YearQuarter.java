573c158a7bfdf1f06e5c6596a6923417d6b01f3f
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-30 17:28:05 -0500

    Create YearQuarter.java

---
+++ codewars/8 kyu/YearQuarter.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/java
+
+import static java.lang.Math.ceil;
+
+public class Kata {
+    public static int quarterOf(int month) {
+        return (int) ceil(month / 3.0);
+    }
+}
